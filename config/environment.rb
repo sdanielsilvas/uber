9c931caa2cc57de54a1ff8c239b5cea1ed521de8
sdanielsilvas <sdanielsilvas@gmail.com>
2016-12-22 16:43:42 -0500

    sendgrid heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'https://desolate-meadow-84025.herokuapp.com' }
+#config.action_mailer.default_url_options = { :host => 'https://desolate-meadow-84025.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :user_name => 'Intergrupo',
   :password => 'Intergrupo2016',
