cf81e3c34e111c73136ec36786d99bb65f537559
sdanielsilvas <sdanielsilvas@gmail.com>
2016-12-22 16:33:53 -0500

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'localhost' }
 ActionMailer::Base.smtp_settings = {
   :user_name => 'Intergrupo',
   :password => 'Intergrupo2016',
