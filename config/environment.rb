49d1b77981203998931a225563a5145a60be333e
ssilva <ssilva@intergrupo.com>
2017-07-10 10:39:21 -0400

    email de produccion

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 #config.action_mailer.default_url_options = { :host => 'https://desolate-meadow-84025.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'ricardo.pineda',
-  :password => 'send123*',
+  :user_name => 'sdanielsilvas123',
+  :password => 'send1234',
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
